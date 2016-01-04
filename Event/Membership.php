60961c9e454c75d799fc455c38d0c0d3807671e7
Andrew Wolder <awolder@cisco.com>
2016-01-04 22:42:43 +0100

    Add Create Membership Function

--- Event/Membership.php
+++ Event/Membership.php
@@ -17,18 +17,24 @@ class Membership  {
 
 	}
 	
+	public function getBaseHeaders()
+	{
+		return array('Authorization' => $this->oauth->getStoredToken(),'content-type'  => 'application/json');
+	}
+	
+	public function getRefreshHeaders()
+	{
+		return array('Authorization' => $this->oauth->getNewToken(),'content-type'  => 'application/json');
+	}
+	
 	public function getMemberships($roomId = null, $personId = null, $personEmail = null, $max = null)
 	{
-		
-	    $baseHeaders    = array('Authorization' => $this->oauth->getStoredToken(),'content-type'  => 'application/json');
-	    $refreshHeaders = array('Authorization' => $this->oauth->getNewToken(),'content-type'  => 'application/json');
 		$queryParams    = array("roomId" => $roomId, "personId" => $personId, "personEmail" => $personEmail, "max" => $max );
 		
 		$client  = new Client();
-
 		try{
 		$response = $client->request("GET", self::MEMBERSHIPURI, array(
-				'headers'       => $baseHeaders,
+				'headers'       => $this->getBaseHeaders(),
 				'query'         => $queryParams
 		));
 		} catch (RequestException $e) {
@@ -39,17 +45,48 @@ class Membership  {
 			{
 	
 				$response = $client->request("GET", self::MEMBERSHIPURI, array(
-				'headers'       => $refreshHeaders,
+				'headers'       => $this->getRefreshHeaders(),
 				'query'         => $queryParams
 				));
 			}
 	
 		}
-		
-		$jsonResponse =  json_decode($response->getBody());
-		
-		return $jsonResponse;
+		return json_decode($response->getBody());	
+	}
 	
+	public function createMembership($roomId = null, $personId = null, $personEmail = null, $isModerator = FALSE)
+	{
+		$queryParams    = array("roomId" => $roomId, "personId" => $personId, "personEmail" => $personEmail, "isModerator" => $isModerator );
+		$roomJson = '{"roomId": "'.$roomId.'"';
+	    	if ($personId != null){
+			$roomJson .= ',"personId": "'.$personId.'"';
+			}
+			if ($personEmail != null){
+			$roomJson .= ',"personEmail": "'.$personEmail.'"';
+			}
+		$roomJson .= ',"isModerator": '.$isModerator.' }';
+		
+		$client  = new Client();
+		try{
+			$response = $client->request("POST", self::MEMBERSHIPURI, array(
+					'headers'       => $this->getBaseHeaders(),
+					'body'          => $roomJson
+			));
+		} catch (RequestException $e) {
+		
+			$statusCode = $e->getResponse()->getStatusCode();
+		
+			if ($statusCode == '401')
+			{
+		
+				$response = $client->request("GET", self::MEMBERSHIPURI, array(
+						'headers'       => $this->getRefreshHeaders(),
+						'body'          => $roomJson
+				));
+			}
+		
+		}
+		return json_decode($response->getBody());	
 	}
 	
 
