02212000011bcccd9476ce1512cf4c7ede034cfe
Andrew Wolder <awolder@cisco.com>
2016-01-06 16:56:04 +0100

    Add get Conversation Function

--- Spark.php
+++ Spark.php
@@ -2,8 +2,6 @@
 
 namespace CiscoSystems\SparkBundle;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use CiscoSystems\SparkBundle\Event\Room;
 use CiscoSystems\SparkBundle\Event\Membership;
 use CiscoSystems\SparkBundle\Event\Message;
@@ -11,6 +9,7 @@ use CiscoSystems\SparkBundle\Event\People;
 use CiscoSystems\SparkBundle\Event\WebHook;
 use CiscoSystems\SparkBundle\Authentication\Oauth;
 
+/* The Spark Class is meant to store common actions which might be performed through the API */
 
 class Spark 
 {
@@ -35,7 +34,14 @@ class Spark
 	}
 	
 	
-	
+	/* Description: Function creates a new room, and adds the API user by default.  Setting Moderated to TRUE
+	 * sets the API user as moderator.  Adding a defaultUserEmail adds an additional user to the room, which is
+	 * highly recommended, or access could be lost to the room (unless you store the resulting room id).
+	 * Param  String  $title
+	 * Param  Boolean $moderated
+	 * Param  String  $defaultUserEmail
+	 * return String 
+	 */
 	public function createRoom($title = "Default Room Name", $moderated = FALSE, $defaultUserEmail = '')
 	{
 		if ('' == $defaultUserEmail)
@@ -45,12 +51,13 @@ class Spark
 		
 		if ($room->id)
 		{
+			if ('' != $defaultUserEmail){
 			$memberOptions = array();
 		    $memberOptions['personEmail'] = $defaultUserEmail;
-		    $memberOptions['isModerator'] = FALSE;
-
-			$addMember = $this->membership->createMembership($room->id, $memberOptions );
-			if (isset($addMember->id)){
+		    $addMember = $this->membership->createMembership($room->id, $memberOptions );
+			}
+		    
+			if (isset($addMember->id) && $moderated == TRUE){
 			$this->lockRoom($room->id);
 			}
 			
@@ -58,7 +65,9 @@ class Spark
 		return $room->id;
 	}
 
-	/* This function sets a person as moderator.  Default is to set the API user as moderator */
+	/* This function sets a person as moderator by email address.  If email is empty
+	 * then the API user will become moderator.  
+	 */
 	public function lockRoom($sparkId = '', $personEmail = '')
 	{
 	    $membershipOptions = array();
@@ -72,8 +81,54 @@ class Spark
 		$membershipArray = $this->membership->getMemberships($membershipOptions);
 		$membershipId    = $membershipArray->items[0]->id;
 		
-		$addMod          = $this->membership->updateMembership($membershipId, TRUE);
-	    return $addMod;
+	    return $this->membership->updateMembership($membershipId, TRUE);
+	}
+	
+	/* This function removes a person as moderator by email address.  If email is empty
+	 * then the API user will be removed as moderator - if already a moderator.
+	 */
+	public function unlockRoom($sparkId = '', $personEmail = '')
+	{
+		$membershipOptions = array();
+		$membershipOptions['roomId'] = $sparkId;
+		if ('' != $personEmail){
+			$membershipOptions['personEmail'] = $personEmail;
+		} else {
+			$membershipOptions['personId']    = $this->oauth->getMachineId();
 		}
+		 
+		$membershipArray = $this->membership->getMemberships($membershipOptions);
+		$membershipId    = $membershipArray->items[0]->id;
+	
+		return $this->membership->updateMembership($membershipId, FALSE);
+	}
+	
+	/* Description: gets the conversation stream for a given room, where the text key has a value.  If text is empty,
+	 * the row may contain a file reference. Files calls will be in a separate function.
+	 */
+	public function getConversation($sparkId = '', $options = array())
+	{
+		
+		$getMessages = $this->message->getMessages($sparkId, $options );
+		
+		if (isset($getMessages->items) && count($getMessages->items) > 0)
+		{
+			$output = array();
+			foreach ($getMessages->items as $items)
+			{
+				if (isset($items->text) && $items->text != '')
+				{
+					$convo = array();
+					$convo['text'] 			= $items->text;
+					$convo['created'] 		= $items->created;
+					$convo['personEmail'] 	= $items->personEmail;
+				$output[] = $convo;
+				}
+			}
+			return $output;	
+		} else {
+			return $getMessages;
+		}
+	}
 
 }
