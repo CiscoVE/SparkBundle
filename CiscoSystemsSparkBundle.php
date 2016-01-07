06c192745809afe422fc3ea12400bf8e436cc596
Andrew Wolder <awolder@cisco.com>
2016-01-07 16:42:11 +0100

    Remove Kernel Interface dependency

--- CiscoSystemsSparkBundle.php
+++ CiscoSystemsSparkBundle.php
@@ -3,15 +3,7 @@
 namespace CiscoSystems\SparkBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 class CiscoSystemsSparkBundle extends Bundle
 {
-    protected $kernel;
-
-    public function __construct( KernelInterface $kernel )
-    {
-        $this->kernel = $kernel;
-    }
-
 }
