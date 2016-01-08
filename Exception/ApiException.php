2e29c1e2d20f5d3b341fa09e8e33e56e13e54499
Andrew Wolder <awolder@cisco.com>
2016-01-08 23:35:38 +0100

    add object array to exception class

--- Exception/ApiException.php
+++ Exception/ApiException.php
@@ -14,33 +14,33 @@ class ApiException extends Exception
 		
 		switch ($code) {
 			case 200:
-				return array("statusCode" => 200, "statusMessage" => "OK");
+				return (object)array("statusCode" => 200, "statusMessage" => "OK");
 				break;
 			case 204:
-				return array("statusCode" => 204, "statusMessage" => "Deleted OK");
+				return (object)array("statusCode" => 204, "statusMessage" => "Deleted OK");
 				break;
 			case 400:
-				return array("statusCode" => 400, "statusMessage" => "The request was invalid or cannot be otherwise served.");
+				return (object)array("statusCode" => 400, "statusMessage" => "The request was invalid or cannot be otherwise served.");
 				break;
 			case 401: 
-				return array("statusCode" => 401, "statusMessage" => "Authentication credentials were missing or incorrect.");
+				return (object)array("statusCode" => 401, "statusMessage" => "Authentication credentials were missing or incorrect.");
 				break;
 			case 403:
-				return array("statusCode" => 403, "statusMessage" => "The request is understood, but it has been refused or access is not allowed.");
+				return (object)array("statusCode" => 403, "statusMessage" => "The request is understood, but it has been refused or access is not allowed.");
 				break;
 			case 404:
-				return array("statusCode" => 404, "statusMessage" => "The URI requested is invalid or the resource requested, such as a user, does not exist. Also returned when the requested format is not supported by the requested method.
+				return (object)array("statusCode" => 404, "statusMessage" => "The URI requested is invalid or the resource requested, such as a user, does not exist. Also returned when the requested format is not supported by the requested method.
 	");
 				break;
 			case 409:
-				return array("statusCode" => 409, "statusMessage" => "The request could not be processed because it conflicts with some established rule of the system. For example, a person may not be added to a room more than once.
+				return (object)array("statusCode" => 409, "statusMessage" => "The request could not be processed because it conflicts with some established rule of the system. For example, a person may not be added to a room more than once.
 	");
 				break;				
 			case 500:
-				return array("statusCode" => 500, "statusMessage" => "Something went wrong on the server.");
+				return (object)array("statusCode" => 500, "statusMessage" => "Something went wrong on the server.");
 				break;
 			case 503:
-				return array("statusCode" => 503, "statusMessage" => "Server is overloaded with requests. Try again later.");
+				return (object)array("statusCode" => 503, "statusMessage" => "Server is overloaded with requests. Try again later.");
 				break;
 		}
 
